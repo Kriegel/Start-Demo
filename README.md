c922fb603da6f167aa300e24dd68145398daa446
Kriegel <peter.kriegel@mailbox.org>
2018-02-11 16:20:58 +0100

    added V.3.5.0 edited README.md

--- README.md
+++ README.md
@@ -1,24 +1,28 @@
 # Start-Demo
+
+ A script to present prepared "canned" Demos of PowerShell functionality
+
 The famous Start-Demo script from Jeffrey Snover ; improved ; to present canned Demos of PowerShell Scripts
 
 It completely transforms the experience of giving a PowerShell demo in the console, allowing you to focus on your messages instead of typing. It is REAL in the sense that the commands REALLY run, the script just eliminates your typing.
 <pre>
 .SYNTAX
 
-Start-Demo.ps1 [[-file] &lt;Object&gt;] [[-command] &lt;int&gt;] [[-promptColor] &lt;ConsoleColor&gt;] [[-commandColor] &lt;ConsoleColor&gt;] [[-commentColor] &lt;ConsoleColor&gt;] [[-AutoSpeed] &lt;int&gt;] [-FullAuto] [-NoPauseAfterExecute]
+
+Start-Demo.ps1 [[-file] &lt;Object&gt;] [[-command] &lt;int&gt;] [[-promptColor] &lt;ConsoleColor&gt;] [[-commandColor] &lt;ConsoleColor&gt;] [[-commentColor] &lt;ConsoleColor&gt;] [[-backgroundColor] &lt;ConsoleColor&gt;] [[-StartDelay] &lt;int&gt;] [[-AutoSpeed] &lt;int&gt;] [[-DelayAfterComment] &lt;int&gt;] [-SkipInstructions] [-FullAuto] [-NoPauseAfterExecute] [-UseMyPrompt] [-SkipAddTheEndLine] [-SkipAddDemoTime]
+
+.SYNOPSIS
+    A script to present prepared "canned" Demos of PowerShell functionality
 
 .DESCRIPTION
 
-• You can specify which file you want to demo (it defaults to “.\demo.txt”) use the -file parameter to change.
-• You can specify which command (line) to start with (it defaults to 0)
-• You can run the demo full automaticallyl by use of the switch
-    -FullAuto
-    and set the speed of the autorun in second between commands with the Integer:
-        -AutoSpeed
-• You can skip the pause after the execute of a command by using the Switch:
-    -NoPauseAfterExecute
-• It shows you the command (both at the prompt and in the Window Title [for the folks at the back of the room) and waits for input.
- • If your input is &lt;CR&gt; (the "Enter" key), it runs the command else you can provide other input and it will do other actions.
+A script to present prepared "canned" Demos of PowerShell functionality
+
+It completely transforms the experience of giving a PowerShell demo in the console, allowing you to focus on your messages instead of typing. It is REAL in the sense that the commands REALLY run, the script just eliminates your typing.
+
+It shows you the command (both at the prompt and in the Window Title [for the folks at the back of the room) and waits for input.
+
+If your input is &lt;CR&gt; (the "Enter" key), it runs the command else you can provide other input and it will do other actions.
    By input of a char ou can:
         (?) Show the help
         (n) Goto Next command
@@ -31,39 +35,90 @@ Start-Demo.ps1 [[-file] &lt;Object&gt;] [[-command] &lt;int&gt;] [[-promptColor]
         (f) Find lines by string
         (a) Auto Execute mode (demo runs full automatically)
         (c) Clear Screen
-• Check your time. It displays how many minutes and seconds since the start of the demo.
-   This information is also displayed in the Window title on an ongoing basis.
- • Colors are control via script input variables (so you can easily change them for your environment)
-    -promptColor
-    -commandColor
-    -commentColor
-    allowed Keyword (colors) are :
-        Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red
-, Magenta, Yellow, White
 
- • Timecheck now tells you where you are in the demo: You are at line 10 of 29
+Check your demo duration time.
+It displays how many minutes and seconds since the start of the demo.
+This information is also displayed in the Window title on an ongoing basis.
+
+ Line Numbers, tells you where you are in the demo:
+ You are at line 10 of 29
+
+
+.PARAMETER file
+    You can specify which file you want to demo (it defaults to “.\demo.txt”)
+
+ .PARAMETER command
+    You can specify which command (line) to start with (it defaults to 0)
+
+ .PARAMETER promptColor
+    Set the color of the promt inside the demo
+
+    valid values are the following keywords out of the .NET enumeration [System.ConsoleColor]
+
+    Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red
+
+ .PARAMETER commandColor
+    Set the color of a command inside the demo
+    for vald values see parameter -promptColor !
+
+ .PARAMETER commentColor
+    Set the color of a commend line inside the demo
+    for vald values see parameter -promptColor !
+
+ .PARAMETER backgroundColor
+    Set the color of the background of all writings inside the demo
+    for vald values see parameter -promptColor !
+
+ .PARAMETER StartDelay
+    Set a pause time bevor the demo starts in seconds
+
+ .PARAMETER AutoSpeed
+    set the speed of the autorun in second between each command
+    see even parameter FullAuto!
+
+ .PARAMETER DelayAfterComment
+    Set the Time of pause after each command execution in seconds
+
+ .PARAMETER SkipInstructions
+    skips the instruction information showed in the console befor a demo starts
+
+ .PARAMETER FullAuto
+    You can run the demo full automaticallyl by use of this switch
+    to set the speed see parameter -AutoSpeed !
+
+ .PARAMETER NoPauseAfterExecute
+    You can skip the pause after the execution of aech command by using this Switch
+
+ .PARAMETER UseMyPrompt
+    uses the original promt function out of the function: drive instead of the prepared promt defined inside the Start-Demo.ps1
+
+ .PARAMETER SkipAddTheEndLine
+    the default is to add a Line "Write-Host 'The End'" at the end of each demo
+    to prevent that the console closes after the last executed command.
+    Use this switch to not add this line
+
+ .PARAMETER SkipAddDemoTime
+    use this switch if you do not like to see the demo duration time inside the console window.
+
 
 .EXAMPLE
 
-    Full example to start a Demo with the script Start-Demo.ps1
-    executing the demo file: 'c:\demos\mydemo.txt'
-    on command 5
-    full automatically with the speed of 6 seconds between each commands
-    and no pause after execution
+    Full example to start a Demo
 
-    .\Start-Demo -file 'c:\demos\mydemo.txt' -command 5 -promptColor 'yellow' -commandColor 'white' -commentColor 'green' -FullAuto -AutoSpeed 6 -NoPauseAfterExecute
+    C:\> Start-Demo.ps1 -file 'C:\demo\MyDemo.txt' -command 6 -promptColor 'yellow' -commandColor 'white' -commentColor 'green' -backgroundColor 'black' -StartDelay 12 -AutoSpeed 7 -DelayAfterComment 2 -SkipInstructions -FullAuto -NoPauseAfterExecute -UseMyPrompt -SkipAddTheEndLine -SkipAddDemoTime
+
 
 .EXAMPLE
 
     minimal example to start a Demo with the script Start-Demo.ps1
     expecting that a file called "demo.txt" lies in the same folder like the script!
 
-    .\Start-Demo
+    C:\demo>  Start-Demo.ps1
 
 </pre>
 ## Version
 
-V.3.0.0 to 3.3.3
+V.3.5.0
 <pre>
 overhaul of Jeffrey Snover's original Start-Demo script by Joel "Jaykul" Bennett on
     http://poshcode.org  (retired repository of PowerShell scripts)
@@ -73,16 +128,26 @@ overhaul of Jeffrey Snover's original Start-Demo script by Joel "Jaykul" Bennett
     for other history see script!
 </pre>
 
-• V.1.1.0 to 2.9.9.
+V.3.3.3
+<pre>
+overhaul of Jeffrey Snover's original Start-Demo script by Joel "Jaykul" Bennett on
+    http://poshcode.org  (retired repository of PowerShell scripts)
+
+    !!!!! JAYKUL CHANGED IT FROM A FUNCTION TO A SCRIPT !!!!!!!
+
+    for other history see script!
+</pre>
+
+• V.2.9.9 from 1.1.0.
 <pre>
 Lost in space ....
 </pre>
 
 • V.1.1.0
 <pre>
-    Published by Jeffrey Snover at
-    http://blogs.msdn.com/b/powershell/archive/2007/03/06/improved-start-demo-script.aspx
-    https://blogs.msdn.microsoft.com/powershell/2007/06/03/new-and-improved-start-demo/
+Published by Jeffrey Snover at
+http://blogs.msdn.com/b/powershell/archive/2007/03/06/improved-start-demo-script.aspx
+https://blogs.msdn.microsoft.com/powershell/2007/06/03/new-and-improved-start-demo/
 
 • Fix bug for demo files that have a single line
 • Intro text explains that this is a REAL demo
@@ -102,7 +167,7 @@ https://www.powershellgallery.com/packages/Start-Demo/1.0.1/DisplayScript
 </pre>
 • V1.0.0
 <pre>
-    Inintial release 03.March.2007 V.1.0.0
-    This script was originally published by Jeffrey Snover at
-    https://blogs.msdn.microsoft.com/powershell/2007/03/03/start-demo-help-doing-demos-using-powershell/
+Inintial release 03.March.2007 V.1.0.0
+This script was originally published by Jeffrey Snover at
+https://blogs.msdn.microsoft.com/powershell/2007/03/03/start-demo-help-doing-demos-using-powershell/
 </pre>
--- Start-Demo.ps1
+++ Start-Demo.ps1
@@ -1,4 +1,5 @@
 ## Start-Demo.ps1
+## http://poshcode.org/6399
 ##################################################################################################
 ## This is an overhaul of Jeffrey Snover's original Start-Demo script by Joel "Jaykul" Bennett
 ##
@@ -9,7 +10,22 @@
 ## I've also been adding features as I come across needs for them, and you'll contribute your
 ## improvements back to the PowerShell Script repository as well.
 ##################################################################################################
-## Revision History (version 3.3)
+## Revision History (version 3.5.0)
+## 3.5.0 Added:    StartDelay parameter added
+##       Added:	   SkipInstructions parameter added
+##       Added:    The help section 'Improving ideas' added
+##       Addde:    DelayAfterComment switch added, add wait time after write comments to screen
+##       Fixed:    Some comments to code added
+##       Fixed:    Restoring oryginal prompt corrected
+## 3.4.1 Fixed:    Switches SkipAddTheEndLine and SkipAddDemoTime corrected
+## 3.4.0 Fixed:    FullAuto mode corrected
+##       Fixed:    Small corrections of code based on PSScriptAnalyzer 1.6.0 sugestions
+##       Added:    Console window title will be set back after a demo end
+##       Added:    Own custom prompt can be used under demo
+##       Added:    SkipAddTheEndLine switch to don't display 'The end' line
+##       Added:    SkipAddDemoTime switch to don't display start/end demo and demo duration in
+##                 a PowerShell console window
+##       Added:    backgroundColor parameter added - now PowerShell console don't need to be black
 ## 3.3.3 Fixed:    Script no longer says "unrecognized key" when you hit shift or ctrl, etc.
 ##       Fixed:    Blank lines in script were showing as errors (now printed like comments)
 ## 3.3.2 Fixed:    Changed the "x" to match the "a" in the help text
@@ -31,20 +47,41 @@
 ##     - Added:    Automatically append an empty line to the end of the demo script
 ##                 so you have a chance to "go back" after the last line of you demo
 ##################################################################################################
-##
-param(
+## Improving ideas
+## - convert script to function
+## - add padding zero(s) to command number
+## - remove char '?' from the prompt in the FullAuto mode
+##################################################################################################
+
+param (
     $file = ".\demo.txt",
     [int]$command = 0,
     [System.ConsoleColor]$promptColor = "Yellow",
     [System.ConsoleColor]$commandColor = "White",
     [System.ConsoleColor]$commentColor = "Green",
+    [System.ConsoleColor]$backgroundColor = "black",
+    [int]$StartDelay,
+    [switch]$SkipInstructions,
     [switch]$FullAuto,
     [int]$AutoSpeed = 3,
-    [switch]$NoPauseAfterExecute
+    [int]$DelayAfterComment = 1,
+    [switch]$NoPauseAfterExecute,
+    [switch]$UseMyPrompt, #To customize change definition in the function Prompt (below)
+    [switch]$SkipAddTheEndLine,
+    [switch]$SkipAddDemoTime
 )
 
 $RawUI = $Host.UI.RawUI
 $hostWidth = $RawUI.BufferSize.Width
+$hostTitle = $RawUI.WindowTitle
+
+If ($UseMyPrompt.ispresent) {
+    $OryginalPrompt = Get-Content Function:\prompt
+    Prompt
+}
+
+# More about constructing prompts here: https://technet.microsoft.com/en-us/library/hh847739.aspx
+Function prompt { "[PS] >" }
 
 # A function for reading in a character
 function Read-Char() {
@@ -59,38 +96,54 @@ function Read-Char() {
     return $inChar.Character
 }
 
+
+
 function Rewind($lines, $index, $steps = 1) {
     $started = $index;
     $index -= $steps;
     while (($index -ge 0) -and ($lines[$index].Trim(" `t").StartsWith("#"))) {
         $index--
     }
-    if ( $index -lt 0 ) { $index = $started }
+    if ($index -lt 0) { $index = $started }
     return $index
 }
 
 $file = Resolve-Path $file
-while (-not(Test-Path $file)) {
+while (-not (Test-Path $file)) {
     $file = Read-Host "Please enter the path of your demo script (Crtl+C to cancel)"
     $file = Resolve-Path $file
 }
 
 Clear-Host
 
-$_lines = Get-Content $file
-# Append an extra (do nothing) line on the end so we can still go back after the last line.
-$_lines += "Write-Host 'The End'"
-$_starttime = [DateTime]::now
-$FullAuto = $false
+If ( $StarDelay -gt 0 ) {
+    Start-Sleep -seconds $StartDelay
+}
 
-Write-Host -nonew -back black -fore $promptColor $(" " * $hostWidth)
-Write-Host -nonew -back black -fore $promptColor @"
-<Demo Started :: $(split-path $file -leaf)>$(' ' * ($hostWidth -(18 + $(split-path $file -leaf).Length)))
+$_lines = Get-Content $file
+
+If (-not $SkipAddTheEndLine.IsPresent) {
+    # Append an extra (do nothing) line on the end so we can still go back after the last line.
+    $_lines += "Write-Host 'The End'"
+}
+
+$_starttime = [DateTime]::now
+
+#Overwrite original prompt (?)
+Write-Host -nonew -back $backgroundColor -fore $promptColor $(" " * $hostWidth)
+
+If (-not $SkipAddDemoTime.IsPresent) {
+    Write-Host -nonew -back $backgroundColor -fore $promptColor @"
+<Demo Started :: $(split-path $file -leaf)>$(' ' * ($hostWidth - (18 + $(split-path $file -leaf).Length)))
 "@
-Write-Host -nonew -back black -fore $promptColor "Press"
-Write-Host -nonew -back black -fore Red " ? "
-Write-Host -nonew -back black -fore $promptColor "for help.$(' ' * ($hostWidth -17))"
-Write-Host -nonew -back black -fore $promptColor $(" " * $hostWidth)
+}
+
+If ( -not ($SkipInstructions.IsPresent -or $FullAuto.IsPresent) ) {
+    Write-Host -nonew -back $backgroundColor -fore $promptColor "Press"
+    Write-Host -nonew -back $backgroundColor -fore Red " ? "
+    Write-Host -nonew -back $backgroundColor -fore $promptColor "for help.$(' ' * ($hostWidth - 17))"
+    Write-Host -nonew -back $backgroundColor -fore $promptColor $(" " * $hostWidth)
+}
 
 # We use a FOR and an INDEX ($_i) instead of a FOREACH because
 # it is possible to start at a different location and/or jump
@@ -98,20 +151,25 @@ Write-Host -nonew -back black -fore $promptColor $(" " * $hostWidth)
 for ($_i = $Command; $_i -lt $_lines.count; $_i++) {
     # Put the current command in the Window Title along with the demo duration
     $Dur = [DateTime]::Now - $_StartTime
-    $RawUI.WindowTitle = "$(if($dur.Hours -gt 0){'{0}h '})$(if($dur.Minutes -gt 0){'{1}m '}){2}s   {3}" -f
+    $RawUI.WindowTitle = "$(if ($dur.Hours -gt 0) { '{0}h ' })$(if ($dur.Minutes -gt 0) { '{1}m ' }){2}s   {3}" -f
     $dur.Hours, $dur.Minutes, $dur.Seconds, $($_Lines[$_i])
 
     # Echo out the commmand to the console with a prompt as though it were real
     Write-Host -nonew -fore $promptColor "[$_i]$([char]0x2265) "
+
+    # Comments line from file can be write to the console using $commentColor as a text (not executed)
     if ($_lines[$_i].Trim(" ").StartsWith("#") -or $_lines[$_i].Trim(" ").Length -le 0) {
         Write-Host -fore $commentColor "$($_Lines[$_i])  "
+        Start-Sleep -Seconds $DelayAfterComment
         continue
     }
     else {
         Write-Host -nonew -fore $commandColor "$($_Lines[$_i])  "
     }
 
-    if ( $FullAuto ) { Start-Sleep $autoSpeed; $ch = [char]13 } else { $ch = Read-Char }
+    if ($FullAuto.IsPresent) { $FullAutoInt = $true; Start-Sleep $autoSpeed; $ch = [char]13 }
+    else { $ch = Read-Char }
+
     switch ($ch) {
         "?" {
             Write-Host -Fore $promptColor @"
@@ -134,13 +192,13 @@ Running demo: $file
         "p" {
             # Previous
             Write-Host -Fore $promptColor "<Back one Line>"
-            while ($_lines[--$_i].Trim(" ").StartsWith("#")) {}
+            while ($_lines[--$_i].Trim(" ").StartsWith("#")) { }
             $_i-- # back a line, we're gonna step forward when we loop
         }
         "a" {
             # EXECUTE (Go Faster)
             $AutoSpeed = [int](Read-Host "Pause (seconds)")
-            $FullAuto = $true;
+            $FullAutoInt = $true;
             Write-Host -Fore $promptColor "<eXecute Remaining Lines>"
             $_i-- # Repeat this line, and then just blow through the rest
         }
@@ -148,12 +206,18 @@ Running demo: $file
             # Quit
             Write-Host -Fore $promptColor "<Quiting demo>"
             $_i = $_lines.count;
+            #Restore oryginal PowerShell host title
+            $RawUI.WindowTitle = $hostTitle
+            #Restore oryginal PowerShell prompt
+            If ($UseMyPrompt.IsPresent) {
+                Invoke-Expression
+            }
             break;
         }
         "v" {
             # View Source
             $lines[0..($_i - 1)] | Write-Host -Fore Yellow
-            $lines[$_i]        | Write-Host -Fore Green
+            $lines[$_i] | Write-Host -Fore Green
             $lines[($_i + 1)..$lines.Count] | Write-Host -Fore Yellow
             $_i-- # back a line, we're gonna step forward when we loop
         }
@@ -161,7 +225,7 @@ Running demo: $file
             # Time Check
             $dur = [DateTime]::Now - $_StartTime
             Write-Host -Fore $promptColor $(
-                "{3} -- $(if($dur.Hours -gt 0){'{0}h '})$(if($dur.Minutes -gt 0){'{1}m '}){2}s" -f
+                "{3} -- $(if ($dur.Hours -gt 0) { '{0}h ' })$(if ($dur.Minutes -gt 0) { '{1}m ' }){2}s" -f
                 $dur.Hours, $dur.Minutes, $dur.Seconds, ([DateTime]::Now.ToShortTimeString()))
             $_i-- # back a line, we're gonna step forward when we loop
         }
@@ -187,13 +251,13 @@ Running demo: $file
         "f" {
             # Find by pattern
             $match = $_lines | Select-String (Read-Host "search string")
-            if ($match -eq $null) {
+            if ([String]::IsNullOrEmpty($match)) {
                 Write-Host -Fore Red "Can't find a matching line"
             }
             else {
-                $match | % { Write-Host -Fore $promptColor $("[{0,2}] {1}" -f ($_.LineNumber - 1), $_.Line) }
+                $match | ForEach-Object -Process { Write-Host -Fore $promptColor $("[{0,2}] {1}" -f ($_.LineNumber - 1), $_.Line) }
                 if ($match.Count -lt 1) {
-                    $_i = $match.lineNumber - 2  # back a line, we're gonna step forward when we loop
+                    $_i = $match.lineNumber - 2 # back a line, we're gonna step forward when we loop
                 }
                 else {
                     $_i-- # back a line, we're gonna step forward when we loop
@@ -207,10 +271,10 @@ Running demo: $file
         "$([char]13)" {
             # on enter
             Write-Host
-            trap [System.Exception] {Write-Error $_; continue; }
+            trap [System.Exception] { Write-Error $_; continue; }
             Invoke-Expression ($_lines[$_i]) | out-default
-            if (-not $NoPauseAfterExecute -and -not $FullAuto) {
-                $null = $RawUI.ReadKey("NoEcho,IncludeKeyUp")  # Pause after output for no apparent reason... ;)
+            if (-not $NoPauseAfterExecute -and -not $FullAutoInt) {
+                $null = $RawUI.ReadKey("NoEcho,IncludeKeyUp") # Pause after output for no apparent reason... ;)
             }
         }
         default {
@@ -220,55 +284,16 @@ Running demo: $file
     }
 }
 $dur = [DateTime]::Now - $_StartTime
-Write-Host -Fore $promptColor $(
-    "<Demo Complete -- $(if($dur.Hours -gt 0){'{0}h '})$(if($dur.Minutes -gt 0){'{1}m '}){2}s>" -f
-    $dur.Hours, $dur.Minutes, $dur.Seconds, [DateTime]::Now.ToLongTimeString())
-Write-Host -Fore $promptColor $([DateTime]::now)
+If (-not $SkipAddDemoTime.IsPresent) {
+    Write-Host -Fore $promptColor $(
+        "<Demo Complete -- $(if ($dur.Hours -gt 0) { '{0}h ' })$(if ($dur.Minutes -gt 0) { '{1}m ' }){2}s>" -f
+        $dur.Hours, $dur.Minutes, $dur.Seconds, [DateTime]::Now.ToLongTimeString())
+    Write-Host -Fore $promptColor $([DateTime]::now)
+}
 Write-Host
-# SIG # Begin signature block
-# MIIIPgYJKoZIhvcNAQcCoIIILzCCCCsCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUhnYBsMQ+7cZlmxCQp6UPJj6A
-# u0GgggVbMIIFVzCCBD+gAwIBAgIRAO2rPg5HUjL4ofGGpnMP2jwwDQYJKoZIhvcN
-# AQEFBQAwgZUxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJVVDEXMBUGA1UEBxMOU2Fs
-# dCBMYWtlIENpdHkxHjAcBgNVBAoTFVRoZSBVU0VSVFJVU1QgTmV0d29yazEhMB8G
-# A1UECxMYaHR0cDovL3d3dy51c2VydHJ1c3QuY29tMR0wGwYDVQQDExRVVE4tVVNF
-# UkZpcnN0LU9iamVjdDAeFw0wODEwMDYwMDAwMDBaFw0wOTEwMDYyMzU5NTlaMIHE
-# MQswCQYDVQQGEwJVUzEOMAwGA1UEEQwFMTQ2MjMxETAPBgNVBAgMCE5ldyBZb3Jr
-# MRIwEAYDVQQHDAlSb2NoZXN0ZXIxFDASBgNVBAkMC01TIDA4MDEtMTdBMRowGAYD
-# VQQJDBExMzUwIEplZmZlcnNvbiBSZDEaMBgGA1UECgwRWGVyb3ggQ29ycG9yYXRp
-# b24xFDASBgNVBAsMC1NFRURVIFRvb2xzMRowGAYDVQQDDBFYZXJveCBDb3Jwb3Jh
-# dGlvbjCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAK50RXT2KUvECfWZ
-# weqeXzTCykPPRh9nC3Hzur/mmvkQHA8iinnSKX4j19C1/MV0rAEeCU1bF7Sgxvov
-# ORreM1Ye/wEqJLAUP/IGZI/qsmmwasGFGbnuevpA3WieNCr5cFGl8Y5Mx6ejaDFi
-# O0GT9EM6gOGZaEEMRbHZc4qXT7CrWScs4Yur5bBZsowaMk5JkvZgihhnN93QolEW
-# ObmtQZlbBDqLuoL9fUnIexlqqIrC/4h0K8VM26HvqhgGlQF2wf4t9xCHFJiX2F7D
-# B10lef5aXzyPVrvxxrRWyBtCQuL7xdXneRanJaYG3B3kclc+4/6dq9a+s/huXjmE
-# omumgGcCAwEAAaOCAW8wggFrMB8GA1UdIwQYMBaAFNrtZHQUnBQ8q92Zqb1bKE2L
-# PMnYMB0GA1UdDgQWBBT5ITlG5CdiD+nI0uTqnXNGnd44QjAOBgNVHQ8BAf8EBAMC
-# B4AwDAYDVR0TAQH/BAIwADATBgNVHSUEDDAKBggrBgEFBQcDAzARBglghkgBhvhC
-# AQEEBAMCBBAwRgYDVR0gBD8wPTA7BgwrBgEEAbIxAQIBAwIwKzApBggrBgEFBQcC
-# ARYdaHR0cHM6Ly9zZWN1cmUuY29tb2RvLm5ldC9DUFMwQgYDVR0fBDswOTA3oDWg
-# M4YxaHR0cDovL2NybC51c2VydHJ1c3QuY29tL1VUTi1VU0VSRmlyc3QtT2JqZWN0
-# LmNybDA0BggrBgEFBQcBAQQoMCYwJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmNv
-# bW9kb2NhLmNvbTAhBgNVHREEGjAYgRZKb2VsLkJlbm5ldHRAWGVyb3guY29tMA0G
-# CSqGSIb3DQEBBQUAA4IBAQAZxnV+BbJBohpy+wKs6U8hRiPUhDYaijzTyrZontf5
-# PEyBbhAkJFIWauIaq9eSQEJeErXO/zuO6+wY/azBzOTleMM9qdGWHFtfAw5WiIuC
-# 90TzDBSuP7LImZV5Pb6nxRbesDF2U7EM5sBzYSWAMfpBmYRz97EHPW5QNzpBLFJn
-# Dhb/M27rDYh7FVjy1+C5E3glIa0A0q+lcxEtFuUij4JId+oMcfpSgYJZvR1Kvkjd
-# GDAtWCzvALaNFd65kChbrOqcClOCacQRnP9N4DJl/RVNKZtcUcVAyTpvOlJBA5vG
-# OVcsJT4TnSMjPX6d5pXMwcE1oWCUWvK99W+N81DvBBuZMYICTTCCAkkCAQEwgasw
-# gZUxCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJVVDEXMBUGA1UEBxMOU2FsdCBMYWtl
-# IENpdHkxHjAcBgNVBAoTFVRoZSBVU0VSVFJVU1QgTmV0d29yazEhMB8GA1UECxMY
-# aHR0cDovL3d3dy51c2VydHJ1c3QuY29tMR0wGwYDVQQDExRVVE4tVVNFUkZpcnN0
-# LU9iamVjdAIRAO2rPg5HUjL4ofGGpnMP2jwwCQYFKw4DAhoFAKB4MBgGCisGAQQB
-# gjcCAQwxCjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYK
-# KwYBBAGCNwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFNEM/yDz
-# vLFTxZZERqngy81y1rFyMA0GCSqGSIb3DQEBAQUABIIBAIi+6WT4dPUfVZyJ1Efd
-# DzTqYmH2wmcGg8KncjeA2vi+F3+9EPDIMRUSNxDO+l/iAel4uiNE3CNlZDiCfi4v
-# W+nc1TTpen1q33ITFLgFaUvMztgJ1D02NsEIAsGX0Qu9FHYXXGkdx6HiTyy5lfl2
-# sLvmQiINVfHJnn28YhSqpEByebojR6177/VhDqk7h9vvhMhuBP+XWlzO+nwaZ84T
-# BcS8Qb/JkKxkhKYhbsfViQX0Rq4/5MjhjR7ctEJvCfkb0dkY97sHddg4kWGy5amI
-# ydV6EoqjHHUJatdgmCc38KIQVdXvaZyC5rTRg/uEJias4BQq3HLdUQDJ+u5l0EeY
-# MbI=
-# SIG # End signature block
+#Restore oryginal PowerShell host title
+$RawUI.WindowTitle = $hostTitle
+#Restore oryginal PowerShell prompt
+If ($UseMyPrompt.IsPresent) {
+    Invoke-Expression -Command "Function Prompt { $OryginalPrompt }" -ErrorAction SilentlyContinue
+}
